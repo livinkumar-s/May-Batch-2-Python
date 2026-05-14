372baaa662f88a9777c7f34aa8f81d9275793020
LivinkumarStackly <livinkumar@thestackly.com>
2026-05-14 11:26:47 +0530

    Introduction

---
+++ intro.py
@@ -0,0 +1,18 @@
+# These are printing statements
+
+'''print(99)
+print(100)
+print(101)'''
+# print("Hello")
+
+# print(9)
+# print(-9.0)
+# print('''I'm "Batman"''')
+
+# a='6.6'
+# print(type(a))
+
+a=input("Enter your name: ")
+print(a)
+print(a)
+print(a)
