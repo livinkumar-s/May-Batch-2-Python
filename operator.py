59a72b3a2a431667a1221923377c0977000a321f
LivinkumarStackly <livinkumar@thestackly.com>
2026-05-21 11:29:40 +0530

    Operators

---
+++ operator.py
@@ -0,0 +1,59 @@
+# a=19
+# b=7
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(4%5)
+# print(4**0)
+
+# print(2+6-12*9/3)
+
+# Comparison 
+# a=-9
+# b=9
+
+# print(a==b)
+# print(a!=b)
+# print(5<7)
+# print(5>5)
+# print(5<=5)
+# print(5>=15)
+
+# Logical operators
+# print(True and True)
+# print(True and True)
+# print(False and True)
+# print(True or True)
+# print(not not False)
+
+# a=10
+# b=9
+
+# print(a<b or a>=b)
+
+# Assignement 
+
+# a=10
+# a=15
+# a+=5
+# a-=10
+# a/=2
+# a//=2
+# a%=5
+# a**=4
+
+# print(a)
+
+# Bitwise operator 
+a=19
+b=13
+
+# print(a&b)
+# print(a|b)
+# print(~-4)
+# print(a^b)
+# print(44>>3)
+# print(66<<2)
+print(~33)
