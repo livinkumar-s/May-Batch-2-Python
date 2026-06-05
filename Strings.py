8c4f8f9bfc7b67c8fb3d710f02a5c7c2eea8c738
LivinkumarStackly <livinkumar@thestackly.com>
2026-06-05 11:50:05 +0530

    Strings

---
+++ Strings.py
@@ -0,0 +1,67 @@
+# str1="Hello World"
+# print(id(str1))
+# str1=str1+"...!"
+# print(id(str1))]
+
+# lis1=[1,2,3]
+# print(lis1)
+# print(id(lis1))
+# lis1.append(4)
+# print(lis1)
+# print(id(lis1))
+
+# str1="Hello"
+# print(str1*30)
+
+# print("ta" not in "Tamil")
+# print(6 in [1,2,3,4,5,6])
+
+# a=[1,2,3]
+# b=[1,2,3]
+
+# print(a==b)
+# print(a is not b)
+
+
+# print(id(a))
+# print(id(b))
+
+
+# str1="hello565356"
+
+# print(str1.isalpha())
+# print(str1.startswith("helfghjd"))
+# print(str1.endswith("56"))
+
+# print(str1.upper())
+# print(str1.lower())
+# print(str1.title())
+# print(str1.capitalize())
+# print('.'+str1.strip()+'.')
+# print('.'+str1.lstrip()+'.')
+# print('.'+str1.rstrip()+'.')
+# print(str1.replace("hello","Hi"))
+
+# print(str1.split("i"))
+# print("---".join(("Hello","Hi","Welcome")))
+
+# lis1=[1,2,3]
+# print(lis1.index(22))
+
+# print(str1.find("rldgjhrdtgj"))
+
+# I'm Leo
+# I said "Boom"
+
+
+# str='I\'m Leo.\nI said "Boom"'
+
+# str1='Hi\nHello\nWelcome'
+
+# print(str1)
+
+name="Vijay"
+age=45
+
+print("My name is "+name+". I am "+str(age))
+print(f"My name is {name}. I am {age}")
