d5f9af12c06fcd461dc9ad3588ad40d2a3d1b79a
LivinkumarStackly <livinkumar@thestackly.com>
2026-06-09 11:41:58 +0530

    Exception Handling

---
+++ ExceptionHandling.py
@@ -0,0 +1,36 @@
+print(9)
+print(10)
+print(11)
+print(12)
+print(13)
+# if True:
+# print(34)
+
+# a=10
+# print(b)
+# print("Hello"+1)
+# print(int("Hello"))
+# print(4/0)
+
+# try:
+#     print("Hello"+6)
+#     print(int("Hi"))
+# except TypeError:
+#     print("Invalid Expression")
+# except ValueError:
+#     print("Invalid type conversion")
+# finally:
+#     print("Done")
+
+
+# try:
+#     inp1=float(input("Enter: "))
+#     inp2=float(input("Enter: "))
+#     print(inp1/inp2)
+# except ValueError:
+#     print("Please provise valid inputs...!")
+# except ZeroDivisionError:
+#     print("Invalid Expression...!")
+
+
+print(99)
